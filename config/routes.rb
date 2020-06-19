25300419669b93e46f79fb3412cf4f8f49fecd59
Ysatuma <yssatuma@gmail.com>
2020-06-19 12:55:49 +0900

    convert to template
    ユーザーメニューの機能一覧を部分テンプレート化。
    また「マイページ」コンテンツの雛形を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'user_menu#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user_menu, only: [:show]
 end
