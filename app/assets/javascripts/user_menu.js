3ace003f4772108ef08dfba6eb80ce320edc9c11
Ysatuma <yssatuma@gmail.com>
2020-06-20 10:57:27 +0900

    edit user_meny by jquery
    コンテンツ内のメニュー選択による遷移をjuqeyで実装

---
+++ app/assets/javascripts/user_menu.js
@@ -0,0 +1,10 @@
+$(function(){
+
+  $('.user-menu__main__contents__mypage--tab__li').on('click', function(e){
+    $('.user-menu__main__contents__mypage--tab__li').removeClass('selected');
+    $(this).addClass('selected');
+    
+  })
+
+
+})
