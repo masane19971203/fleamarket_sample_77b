6c6c16e2448ce869331fd337cb0002c00e1f51bc
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-20 12:27:08 +0900

    add_column shipping_date_table

--- db/migrate/20200620032500_create_shipping_dates.rb
+++ db/migrate/20200620032500_create_shipping_dates.rb
@@ -1,7 +1,7 @@
 class CreateShippingDates < ActiveRecord::Migration[5.2]
   def change
     create_table :shipping_dates do |t|
-      
+      t.string :text, null: false
       t.timestamps
     end
   end
