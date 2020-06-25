25b029c4b339082893e6bf8ee1fdc941f10a6695
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-25 10:38:52 +0900

    add_comment_products.js

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,12 +2,13 @@ $(function(){
   var htmlText = `<div class="htmlText">入力してください</div>`
 
   var htmlSelect = `<div class="htmlText">選択して下さい</div>`
-
+  // 商品の説明のテキストに入力時、入力した文字数を表示
   $(".contents__form__item__textarea").on('keyup', function() {
     var count = $(this).val().length;
     $('.contents__form__item__count').text(count + '/1000')
   });
 
+  // 入力必須なテキスト欄が未入力時に警告文を表示、入力時に警告文を削除
   $(".insert").on('keyup blur', function() {
     if ($(this).val().length == 0 && !($(this).next('.htmlText').length)) {
       $(this).css({'border-color': 'red'})
@@ -18,6 +19,7 @@ $(function(){
     }
   });
 
+  // セレクト欄で未選択でフォーカスが外れた時に、警告文を表示
   $(".category-select").on('blur', function() {
     if ($(this).val().length == 0 && !($(this).next('.htmlText').length)) {
       $(this).css({'border-color': 'red'})
@@ -25,6 +27,7 @@ $(function(){
     }
   });
 
+  // セレクト欄で選択に、警告文を削除
   $(".category-select").on("change",function() {
     if ( $(this).val() != "" ) {
       $(this).next('.htmlText').remove()
@@ -32,6 +35,7 @@ $(function(){
     } 
   });
 
+  // カテゴリー欄で選択・未選択時に、サブカテゴリー1以下の表示・削除
   $(".products-select").on("change",function() {
     // ↓controller反映時にifの条件の変更をお願いします
     if ( $(this).val() != "" ) {
@@ -42,7 +46,8 @@ $(function(){
       $(".hidden1,.hidden2,.hidden3").val("");
     }
   });
-
+  
+  // サブカテゴリー1欄で選択・未選択時に、サブカテゴリー2以下の表示・削除
   $(".hidden1").on("change",function() {
     // ↓controller反映時にifの条件の変更をお願いします
     if ( $(this).val() != "" ) {
@@ -55,6 +60,7 @@ $(function(){
     }
   });
 
+  // サブカテゴリー2欄で選択・未選択時に、サイズ選択肢の表示・削除
   $(".hidden2").on("change",function() {
     // ↓controller反映時にifの条件の変更をお願いします
     if ( $(this).val() != "" ) {
@@ -66,9 +72,5 @@ $(function(){
       $(".hidden3").next('.htmlText').remove()
       $(".hidden3").val('');
     }
-  });
-
-  $(".hidden3").on("change",function() {
-    $(this).next('.htmlText').remove()
   });
 });
