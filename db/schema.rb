03f60b78256eda92fa8f226703ca2a5cf19387f4
Ysatuma <yssatuma@gmail.com>
2020-06-26 15:22:47 +0900

    set active hash to area model
    areaモデルにactive hashを導入し、データを記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_013832) do
+ActiveRecord::Schema.define(version: 2020_06_26_061731) do
 
   create_table "areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
