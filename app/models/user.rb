4f8897bd056c72256de83d6b1216f09e076d98b9
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-27 14:25:15 +0900

    add_association_to_user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい" }
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい" }
+
+  belongs_to :address
 end
