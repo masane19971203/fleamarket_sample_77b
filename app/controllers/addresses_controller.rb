6ad8092a39562e0fde79576c1c53daf9c4dc9546
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-27 14:27:32 +0900

    update_new,create,update_action_addresses_controller.rb

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,30 @@
 class AddressesController < ApplicationController
   def new
-    @address = Address.new
+    @user = User.find(current_user.id)
+    if @address = Address.find_by(user_id: current_user.id)
+    else
+      @address = Address.new
+    end
+  end
+
+  def create
+    if Address.create(address_params)
+      redirect_to root_path
+    else
+      render 'address#new'
+    end
+  end
+
+  def update
+    if Address.update(address_params)
+      redirect_to root_path
+    else
+      render 'address#new'
+    end
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zip, :area_id, :city, :number, :building, :phonenumber).merge(user_id: current_user.id)
   end
 end
