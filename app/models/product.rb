01edcd60dd99f31ee8b2678c64fb2460a2023af3
fushi68 <65640842+fushi68@users.noreply.github.com>
2020-06-29 11:54:55 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  has_many :pictures
+  accepts_nested_attributes_for :pictures, allow_destroy: true
+  validates :name, :text, :price, :category, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :size
+  belongs_to_active_hash :area
+  belongs_to_active_hash :category
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :shipping_date
+  
 end
