d796ee98cadf56692fdc9e6f0c446b4b6a3a3f91
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-29 14:07:26 +0900

    add_edit,update_action_users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
-  def new
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :comment)
   end
 end
+
