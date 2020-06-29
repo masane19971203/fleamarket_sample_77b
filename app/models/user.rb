7ad19996d17ec086684339d982cb3f6270296113
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-29 19:28:25 +0900

    add_attr_accessor

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: { minimum: 7 }, confirmation: true
+  validates :password, presence: true, length: { minimum: 7 }, confirmation: true, unless: :without_validate
+  attr_accessor :without_validate
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい" }
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい" }
 
   has_one :address
+
 end
