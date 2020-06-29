518160e47ded94ebdd66e1d98520237feaf5e05b
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-29 10:58:13 +0900

    add_validation_to_address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,11 @@
 class Address < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい" }
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい" }
+  validates :zip, presence: true, length: { is: 7 }
+  validates :area_id, presence: true
+  validates :user_id, presence: true
 
   belongs_to :user
 
