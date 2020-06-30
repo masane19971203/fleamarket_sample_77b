78fd55e545aa98ce05cdc8935a4f82f56bdc3fef
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-30 11:40:25 +0900

    create_address_faker

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :address do
-    
+    Faker::Config.locale = :ja
+    first_name {Gimei.first.kanji}
+    last_name {Gimei.last.kanji}
+    first_name_kana {Gimei.first.katakana}
+    last_name_kana {Gimei.last.katakana}
+    city {Gimei.city.kanji }
+    number {Gimei.town.to_s}
+    zip {Faker::Number.number(digits: 7)}
+    phonenumber {Faker::PhoneNumber.phone_number}
+    area_id {Faker::Number.between(from: 1, to: 47)}
   end
 end
