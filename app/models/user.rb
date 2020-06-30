2a6c8cf594df1dca7054d53a1e064e1987ac77f4
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-30 11:40:34 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい" }
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい" }
+  validates :dob, presence: true
 
   has_one :address
 
