ffaa7441f2bc86d32ecd74e23868f3e66a197037
Ysatuma <yssatuma@gmail.com>
2020-06-30 12:15:31 +0900

    make view file
    ビュー画面の作成及びコントローラの仮コーディングを実施。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,36 @@
 class ProductsController < ApplicationController
+  before_action :get_categories, only: [:index, :show]
 
   def index
+
+    # @category = Category.find(params[:id])
+    # @products = Product.new
+    # @children = Category.new
+
+    # # 親カテゴリーの場合
+    # if(@category.ancestry == nil)
+    #   @children = @category.children
+
+    #   @children.each do |child|
+    #     child.children.each do |g_child|
+    #       @products << Product.where(category_id: g_child.id)
+    #     end
+    #   end
+    # else
+    #   # 子カテゴリーの場合
+    #   if(@category.ancestry.to_i >= 1)
+    #     children = @category.children
+    #     @children = children.children
+
+    #     children.each do |child|
+    #       @products << Product.where(category_id: child.id)
+    #     end
+
+    #   # 孫カテゴリー選択の場合
+    #   else
+    #     @products = Product.where(category_id: params[:id])
+    #   end
+    # end 
 
   end
 
@@ -11,7 +41,13 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @categories = Category.where(ancestry: nil)
+   
   end
   
+  private 
+
+  def get_categories
+    @categories = Category.where(ancestry: nil)
+  end
+
 end
