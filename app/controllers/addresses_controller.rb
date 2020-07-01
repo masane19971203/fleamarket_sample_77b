9c221de50f41f29cdc767c45acdf79cf131c0365
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-07-01 09:21:32 +0900

    Update addresses_controller.rb

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,14 +1,16 @@
 class AddressesController < ApplicationController
+  before_action :ensure_correct_user, only: [:new, :create, :update]
+
   def new
-    @user = User.find(current_user.id)
-    if @address = Address.find_by(user_id: current_user.id)
+    @user = User.find(params[:user_id])
+    if @address = Address.find_by(user_id: @user.id)
     else
       @address = Address.new
     end
   end
 
   def create
-    @user = User.find(current_user.id)
+    @user = User.find(params[:user_id])
     @address = Address.new(address_params)
     if @address.save
       redirect_to root_path
@@ -18,7 +20,7 @@ class AddressesController < ApplicationController
   end
 
   def update
-    @user = User.find(current_user.id)
+    @user = User.find(params[:user_id])
     @address = Address.find(params[:id])
     if @address.update(address_params)
       redirect_to root_path
@@ -29,6 +31,12 @@ class AddressesController < ApplicationController
 
   private
   def address_params
-    params.require(:address).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zip, :area_id, :city, :number, :building, :phonenumber).merge(user_id: current_user.id)
+    params.require(:address).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zip, :area_id, :city, :number, :building, :phonenumber).merge(user_id: @user.id)
+  end
+
+  def ensure_correct_user
+    if current_user.id != params[:user_id].to_i
+      redirect_to root_path
+    end
   end
 end
