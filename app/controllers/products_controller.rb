9cb9f592f2a41ee503e68cb212d4a9bd56feb00b
Ysatuma <yssatuma@gmail.com>
2020-07-01 14:31:24 +0900

    set categories
    テーブル内のカテゴリーを選択できる様関連ファイルを作成及び編集した。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,12 +7,33 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
     @product.pictures.new
+
+    @categories = Category.where(ancestry: nil)
+
+    @category1 = []
+    @category2 = []
+    @category3 = []
+
+    @categories.each do |root|
+      # 親レコードを取得
+      @category1.push([root.name, root.id])
+
+      # root.children.each do |child|
+      #   @category2.push([child.name, child.id])
+
+      #   child.children.each do |g_child|
+      #     @category3.push([g_child.name, g_child.id])
+      #   end
+      # end
+    end
+    
+
   end
 
   def create
     @product = Product.new(product_params)
     binding.pry
-    @product.save
+    @product.save!
     
     redirect_to root_path
     
@@ -25,6 +46,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :text, :price, :brand, :status, :category_id, :size_id, :status_id, :postage_id, :area_id, :shipping_date_id, pictures_attributes: [:image])
+    params.require(:product).permit(:name, :text, :price, :brand, :status, :category_id, :status_id, :postage_id, :area_id, :shipping_date_id, pictures_attributes: [:image])
   end
 end
