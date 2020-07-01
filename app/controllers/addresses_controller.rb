a5aad2ec556b83152b55aef2895e6c41db42f423
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-07-01 10:49:57 +0900

    Update addresses_controller.rb

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,8 +1,8 @@
 class AddressesController < ApplicationController
   before_action :ensure_correct_user, only: [:new, :create, :update]
+  before_action :set_user, only: [:new, :create, :update ]
 
   def new
-    @user = User.find(params[:user_id])
     if @address = Address.find_by(user_id: @user.id)
     else
       @address = Address.new
@@ -10,7 +10,6 @@ class AddressesController < ApplicationController
   end
 
   def create
-    @user = User.find(params[:user_id])
     @address = Address.new(address_params)
     if @address.save
       redirect_to root_path
@@ -20,7 +19,6 @@ class AddressesController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:user_id])
     @address = Address.find(params[:id])
     if @address.update(address_params)
       redirect_to root_path
@@ -30,6 +28,7 @@ class AddressesController < ApplicationController
   end
 
   private
+  
   def address_params
     params.require(:address).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zip, :area_id, :city, :number, :building, :phonenumber).merge(user_id: @user.id)
   end
@@ -38,5 +37,9 @@ class AddressesController < ApplicationController
     if current_user.id != params[:user_id].to_i
       redirect_to root_path
     end
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
