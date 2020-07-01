5385f5e706a36ae71509495f3e93ae3fa9d94e56
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-07-01 16:03:27 +0900

    add_routes_products-edit,update,user_index

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,14 @@ Rails.application.routes.draw do
 
   resources :user_menu, only: [:index, :show, :new, :create]
 
-  resources :products, only: [:index, :show, :new, :create]
+  resources :products, except: :destroy
 
   resources :users, only: [:index, :show, :new, :edit, :update]
   resources :users do
     resources :addresses, only: [:new, :create, :update]
+  end
+  resources :users do
+    get "/products", to: "products#user_index"
   end
 
 end
