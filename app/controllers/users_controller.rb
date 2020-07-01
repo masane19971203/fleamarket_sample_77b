44fc6dd3b32aa29e12ac6c2021ace40047db9243
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-07-01 10:48:37 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update]
+
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     @user.update_attributes(user_params)
     if @user.save
       redirect_to root_path
@@ -15,13 +15,19 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:nickname, :comment).merge(without_validate: true)
   end
+
   def ensure_correct_user
     if current_user.id != params[:id].to_i
       redirect_to root_path
     end
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
 
