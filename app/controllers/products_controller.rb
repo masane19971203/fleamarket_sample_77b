fcfd1afc1c83c36af3eb24392301f53fcabdd600
Ysatuma <yssatuma@gmail.com>
2020-07-02 14:44:44 +0900

    fix redirect_to
    出品情報送信後にリダイレクトされない不具合を修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,10 +23,12 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.save
-    
-    redirect_to root_path
-    
+
+    if @product.save  
+      redirect_to root_path
+    else 
+      binding.pry
+    end
   end
 
   def show
