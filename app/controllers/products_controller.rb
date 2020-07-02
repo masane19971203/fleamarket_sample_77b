56298282f783226794ec0628326ebb715c578a69
Ysatuma <yssatuma@gmail.com>
2020-07-02 23:14:00 +0900

    redirect to show action
    showアクションに遷移できるよう、商品の情報表示及び詳細画面へのリンクを作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,10 +27,14 @@ class ProductsController < ApplicationController
       else
         
       end
+      # 対応する写真一覧を取得
+      @pictures = Picture.group(:product_id).where(product_id: @products.ids)
 
     else
       @view_all = true
     end
+
+
   end
 
   def new
@@ -55,25 +59,28 @@ class ProductsController < ApplicationController
     @product.save!
     
     redirect_to root_path
-    
   end
 
   def show
-    binding.pry
-    @category = Category.find(params[:id])
+    
+    @category = Category.find(params[:category_id])
     @level = @category.depth
+
+    # 対応する写真一覧を取得
+    @products = Product.where(category_id: params[:category_id])
+    @pictures = Picture.group(:product_id).where(product_id: @products.ids)
   end
   
   private 
 
   def get_categories
     @categories = Category.where(ancestry: nil)
-
+  end
+  
   def user_index
     @categories = Category.where(ancestry: nil)
-    @products = Product.where(user_id: params[:user_id])
+    @products = Product.group(:product_id).where(user_id: params[:user_id])
   end
-  private
 
   def product_params
     params.require(:product).permit(:name, :text, :price, :brand, :status, :category_id, :size_id, :status_id, :postage_id, :area_id, :shipping_date_id, pictures_attributes: [:image]).merge(user_id: current_user.id)
