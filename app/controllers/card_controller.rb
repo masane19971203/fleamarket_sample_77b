65431c5c7da7fd05ea517a086cca39cb1539fc74
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-07-03 10:25:38 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -29,6 +29,26 @@ class CardController < ApplicationController
     end
   end
 
-  def show
+  def show #Cardのデータpayjpに送り情報を取り出します
+    @card = Card.where(user_id: current_user.id).first
+    if @card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
+  end
+
+  def destroy #PayjpとCardデータベースを削除します
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+      redirect_to action: "new"
   end
 end
