093ef69cda4e5a2ed4e6512fa0f46fb298b8ccaa
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-07-03 14:30:01 +0900

    add_card_factory

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :card do
-    
+    user_id {Faker::Number.non_zero_digit}
+    customer_id {Faker::Blockchain::Aeternity}
+    card_id {Faker::Blockchain::Aeternity}
   end
 end
