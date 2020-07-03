876a4fa4ebef0f6b39286f2c9846cc8881de9c28
Ysatuma <yssatuma@gmail.com>
2020-07-03 20:10:57 +0900

    toppage contents

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -2,6 +2,8 @@ class ToppageController < ApplicationController
   
   def index
     @categories = Category.where(ancestry: nil)
+    @products =  Product.all.order(id:"DESC").limit(4)
+    @pictures = Picture.group(:product_id).where(product_id: @products.ids)
   end
   
 end
