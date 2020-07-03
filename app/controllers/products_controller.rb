79f714fa9eb3dfaf8cfc88933769a5596947e36d
Ysatuma <yssatuma@gmail.com>
2020-07-03 09:53:54 +0900

    add comment to product controller
    productコントローラにコメントアウトの追加及び空白行の編集を実施

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,13 @@
 class ProductsController < ApplicationController
   before_action :get_categories, only: [:index, :show]
 
+  # 商品一覧画面の表示
   def index
-    @categories = Category.where(ancestry: nil)
-    
+    @categories = Category.where(ancestry: nil)   
     @products = Product.new
     @children = Category.new
     @level = nil
+
     # 特定のカテゴリのみ閲覧
     unless(params[:all_view] == 'true')
       @view_all = false
@@ -18,25 +19,28 @@ class ProductsController < ApplicationController
         @children = @category.children
         @products = Product.where(category_id: @category.indirects.ids)
 
+      # 子カテゴリーの場合  
       when 1
         @children = @category.children
         @products = Product.where(category_id: @children.ids)
        
+      # 孫カテゴリーの場合  
       when 2
         @products = Product.where(category_id: params[:id])
       else
         
       end
-      # 対応する写真一覧を取得
+      # 画像一覧を取得
       @pictures = Picture.group(:product_id).where(product_id: @products.ids)
 
+    # 全カテゴリーを閲覧
     else
       @view_all = true
     end
-
-
   end
 
+
+  # 商品出品画面の表示
   def new
     @product = Product.new
     @product.pictures.new
@@ -50,10 +54,11 @@ class ProductsController < ApplicationController
     # 親レコードを取得
     @categories.each do |root|
       @category1.push([root.name, root.id])
-    end
-    
+    end    
   end
 
+
+  # 出品した商品の出品処理
   def create
     @product = Product.new(product_params)
     @product.save!
@@ -61,16 +66,19 @@ class ProductsController < ApplicationController
     redirect_to root_path
   end
 
-  def show
-    
+
+  # 商品詳細画面の表示
+  def show   
     @category = Category.find(params[:category_id])
     @level = @category.depth
 
-    # 対応する写真一覧を取得
+    # 同一カテゴリの商品及び対応する写真一覧を取得
     @products = Product.where(category_id: params[:category_id])
     @pictures = Picture.group(:product_id).where(product_id: @products.ids)
   end
   
+
+
   private 
 
   def get_categories
