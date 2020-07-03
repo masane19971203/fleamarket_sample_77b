e7d6bff27af294080f378e021a724707a32effaf
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-07-03 14:30:25 +0900

    Update card_spec.rb

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'cardを保存出来る場合' do
+    it 'user_id,customer_id,card_idがあれば保存できる' do
+      card = build(:card)
+      expect(card).to be_valid
+    end
+  end
+  context 'cardを保存できない場合' do
+    it 'user_idが空だと保存できないこと' do
+      card = build(:card, user_id: nil)
+      card.valid?
+      expect(card.errors[:user_id]).to include("can't be blank")
+    end
+    it 'customer_idが空だと保存できないこと' do
+      card = build(:card, customer_id: nil)
+      card.valid?
+      expect(card.errors[:customer_id]).to include("can't be blank")
+    end
+    it 'card_idが空だと保存できないこと' do
+      card = build(:card, card_id: nil)
+      card.valid?
+      expect(card.errors[:card_id]).to include("can't be blank")
+    end
+    it '同じuser_idが既に存在していると保存できないこと' do
+      card = create(:card)
+      card2 = build(:card, user_id: card.user_id)
+      card2.valid?
+      expect(card2.errors[:user_id]).to include("has already been taken")
+    end
+  end
 end
