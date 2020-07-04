f1d0d2d56327f282b0b9edc454daf9f5f25996d6
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-07-04 18:57:58 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,25 @@
 Rails.application.routes.draw do
 
-  get 'card/new'
-  get 'card/show'
   devise_for :users
   root 'toppage#index'
 
-  resources :user_menu, only: [:index, :show, :new, :create]
+  resources :user_menu, only: [:index, :show, :create]
 
-  resources :products, only: [:index,:new, :create, :show]
+  resources :products, only: [:index,:new, :create]
+
+  resources :products, only: :show do
+    post "/card", to: "card#buy"
+  end
+
+  get "/products/show2/:id", to: "products#show2"
 
   resources :users, only: [:index, :show, :new, :edit, :update] do
     resources :addresses, only: [:new, :create, :update]
   end
+  
+  resources :card, only: [:new, :index, :create, :destroy]
 
-  resources :card, only: [:new, :index] do
-    collection do
-      resources :card, only: [:create, :destroy]
-    end
-  end
-  resources :users do 
+  resources :users do
     get "/products", to: "products#user_index"
   end
 
