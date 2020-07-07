cdc79a34e3bc0b4867fe412fd2615e20d5317c7b
Ysatuma <yssatuma@gmail.com>
2020-07-07 18:59:48 +0900

    edit code for production
    本番環境用のコードを修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -158,11 +158,11 @@ class ProductsController < ApplicationController
     if Rails.env.production?
       client = Aws::S3::Client.new(
                              region: 'ap-northeast-1',
-                             access_key_id: Rails.application.credentials.aws[:access_key_id],
-                             secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+                             access_key_id: Rails.application.credentials.aws[:aws_access_key_id],
+                             secret_access_key: Rails.application.credentials.aws[:aws_secret_access_key],
                              )
       @product.pictures.each do |image|
-        binary_data = client.get_object(bucket: 'freemarket-sample-51a', key: image.image_url.file.path).body.read
+        binary_data = client.get_object(bucket: 'f77b-furima-images', key: image.image_url.file.path).body.read
         gon.product_pictures_binary_datas << Base64.strict_encode64(binary_data)
       end
     else
